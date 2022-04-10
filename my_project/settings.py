36804f8835582dba62bb3c8b14c95f150dc0e60f
pepebotika69 <anti_todo@msn.com>
2022-04-10 11:46:33 +0300

    use_asyncio - Fix dockers

--- my_project/settings.py
+++ my_project/settings.py
@@ -92,8 +92,9 @@ else:
             'NAME': os.environ.get('POSTGRES_NAME'),
             'USER': os.environ.get('POSTGRES_USER'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('POSTGRES_DB'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
             'PORT': os.environ.get('POSTGRES_PORT'),
+            'DB': os.environ.get('POSTGRES_DB'),
         }
     }
 
