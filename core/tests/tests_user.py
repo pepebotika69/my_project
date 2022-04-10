4671f29c72f4d4c67479eea30d5f73b3f603dcdf
pepebotika69 <anti_todo@msn.com>
2022-04-10 12:09:01 +0300

    add_tests - Add tests

--- core/tests/tests_user.py
+++ core/tests/tests_user.py
@@ -1,14 +1,22 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-from django.contrib.auth.models import User
 
+class UserTestCase(TestCase):
+    PASSWORD = 'root'
+    USERNAME = 'root'
+    EMAIL = 'root@root.com'
 
-class AnimalTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create(username="root", email='root@root.com')
-        user.set_password('root')
+        user = User.objects.create(username=self.USERNAME, email=self.EMAIL)
+        user.set_password(self.PASSWORD)
         user.save()
 
-    def test_animals_name(self):
-        users = User.objects.filter(username="root")
+    def test_user_exists(self):
+        users = User.objects.filter(username=self.USERNAME)
         self.assertEqual(1, users.count())
+        self.assertTrue(users.exists())
+
+    def test_user_password(self):
+        user = User.objects.get(username=self.USERNAME)
+        self.assertTrue(user.check_password(self.PASSWORD), 'Password incorrect')
