93f5490ea4fe3e8efa1670735142116ebc9849c6
pepebotika69 <anti_todo@msn.com>
2022-04-10 18:18:49 +0300

    pets - Add new view

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from core.views import pet, asyncio
 
 urlpatterns = [
     path('asyncio/', asyncio.index, name='basic_index'),
+    # pet
     path('pet/', pet.index, name='pet_index'),
+    path('pet/all', pet.get_all_pets, name='pet_all'),
 ]
--- core/views/pet.py
+++ core/views/pet.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
+from core.models import Pet
 
-@permission_required('core.view_animal', raise_exception=True)
+
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'pet/index.html', {})
+
+
+@permission_required('core.view_pet', raise_exception=True)
+def get_all_pets(request):
+    return render(request, 'pet/all_pets.html', {
+        'pets': Pet.objects.all()
+    })
