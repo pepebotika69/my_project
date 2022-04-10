90efee32da51a55201fb53b643763856a4161c42
pepebotika69 <anti_todo@msn.com>
2022-04-10 13:16:58 +0300

    add_tests - Check permissions in test

---
+++ core/tests/tests_basic_views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+class BasicViewsTestCase(TestCase):
+    PASSWORD = 'root'
+    USERNAME = 'root'
+    EMAIL = 'root@root.com'
+
+    def setUp(self):
+        user = User.objects.create(username=self.USERNAME, email=self.EMAIL)
+        user.set_password(self.PASSWORD)
+        user.save()
+
+    def test_user_is_forbidden(self):
+        response = self.client.get('/core/basic/')
+        self.assertEqual(response.status_code, 403)
